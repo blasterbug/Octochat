9f41945358ee61824f0827ec24efb956f1d50b9c
Giraudux <giraudux@gmail.com>
2014-09-26 18:25:55 +0200

    fix prefix install path

--- bin/install-boost.sh
+++ bin/install-boost.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-version="1.56.0"
+prefix=`pwd`
+boost_version="1.56.0"
+file_version=`echo ${boost_version} | tr . _`
 
 if [ "$#" -ne 0 ]
 then
@@ -8,28 +10,28 @@ then
     exit 0
 fi
 
-wget http://downloads.sourceforge.net/project/boost/boost/${version}/boost_`echo $version | tr . _`.7z
+wget http://downloads.sourceforge.net/project/boost/boost/${boost_version}/boost_${file_version}.7z
 if [ "$?" -ne 0 ]
 then
     echo "wget failed"
     exit 1
 fi
 
-7z x ./boost_`echo $version | tr . _`.7z
+7z x ./boost_${file_version}.7z
 if [ "$?" -ne 0 ]
 then
     echo "7z failed"
     exit 1
 fi
 
-cd ./boost_`echo $version | tr . _`
+cd ./boost_${file_version}
 if [ "$?" -ne 0 ]
 then
     echo "cd failed"
     exit 1
 fi
 
-./bootstrap.sh --prefix=`pwd` --with-libraries=system,thread,date_time,regex,serialization
+./bootstrap.sh --prefix=${prefix} --with-libraries=system,thread,date_time,regex,serialization
 if [ "$?" -ne 0 ]
 then
     echo "bootstratp failed"
@@ -43,7 +45,8 @@ then
     exit 1
 fi
 
-cd .. && rm -r ./boost_`echo $version | tr . _`*
+cd ..
+rm -r ./boost_${file_version}*
 if [ "$?" -ne 0 ]
 then
     echo "rm failed"
