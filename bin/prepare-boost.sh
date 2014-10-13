670ef123962fadb1b24eab008d8009f77220a55d
Giraudux <giraudux@gmail.com>
2014-10-13 03:17:06 +0200

    new script to get boost according to curl/wget and 7z/tar/unzip

---
+++ bin/prepare-boost.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+var_version_dot=""
+var_version_underscore=""
+var_format=""
+
+function func_download {
+    if command -v curl
+    then
+        curl -O http://downloads.sourceforge.net/project/boost/boost/$var_version_dot/boost_$var_version_underscore.$var_format
+    elif command -v wget
+    then
+        wget http://downloads.sourceforge.net/project/boost/boost/$var_version_dot/boost_$var_version_underscore.$var_format
+    else
+        echo "error" 1>&2
+        exit 1
+    fi
+
+    if [ $? -ne 0 ]
+    then
+        echo "error" 1>&2
+        exit 1
+    fi
+}
+
+if [ $# -ne 1 ]
+then
+    echo "usage" 1>&2
+    exit 1
+fi
+
+if [[ ! $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]
+then
+    echo "error" 1>&2
+    exit 1
+fi
+
+var_version_dot=$1
+var_version_underscore=`echo $var_version_dot | tr . _`
+
+if command -v 7z
+then
+    var_format="7z"
+    func_download
+    7z x boost_$var_version_underscore.$var_format
+elif command -v tar
+then
+    var_format="tar.bz2"
+    func_download
+    tar xf boost_$var_version_underscore.$var_format
+elif command -v unzip
+then
+    var_format="zip"
+    func_download
+    unzip boost_$var_version_underscore.$var_format
+else
+    echo "error" 1>&2
+    exit 1
+fi
+
+if [ $? -ne 0 ]
+then
+	echo "error" 1>&2
+	exit 1
+fi
+
+mv boost_$var_version_underscore boost && cd boost && ./bootstrap.sh
