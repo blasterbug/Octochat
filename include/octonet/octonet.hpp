4c121ae6189a71628edc64b7f1f7493ba4ef6000
Giraudux <giraudux@gmail.com>
2014-11-13 21:42:57 +0100

    set implementations in header

--- include/octonet/octonet.hpp
+++ include/octonet/octonet.hpp
@@ -8,6 +8,7 @@
 
 #include <set>
 
+#include "octoconnect_tcp.hpp"
 #include "octopeer.hpp"
 #include "octoquery.hpp"
 #include "octopeer_observer.hpp"
@@ -16,23 +17,31 @@
 class octonet
 {
     private:
-        //TODO : think about optimal container
         std::set<octoquery_observer*> query_observers;
         std::set<octopeer_observer*> peer_observers;
         //TODO : think about const members
         unsigned short __tcp_port;
         unsigned short __udp_port;
+
     protected:
-        void _notify_query_observers(const octoquery &oq) const;
-        void _notify_peer_observers(const octopeer &op) const;
+        void _notify_query_observers(const octoquery &oq) const {}
+        void _notify_peer_observers(const octopeer &op) const
+        {
+			std::set<octopeer_observer*>::const_iterator it;
+			for(it=peer_observers.begin(); it!=peer_observers.end(); ++it)
+			{
+				(*it)->update_peer(op);
+			}
+		}
+
     public:
-        octonet();
-        ~octonet();
-        bool add_query_observer(octoquery_observer *oq_obs);
-        bool rem_query_observer(octoquery_observer *oq_obs);
-        bool add_peer_observer(octopeer_observer *op_obs);
-        bool rem_peer_observer(octopeer_observer *op_obs);
-        bool send_query(const octopeer &op, const octoquery &oq) const;
+        octonet() {}
+        ~octonet() {}
+        bool add_query_observer(octoquery_observer *oq_obs) { return (query_observers.insert(oq_obs)).second; }
+        bool rem_query_observer(octoquery_observer *oq_obs) { return query_observers.erase(oq_obs) > 0; }
+        bool add_peer_observer(octopeer_observer *op_obs) { return (peer_observers.insert(op_obs)).second; }
+        bool rem_peer_observer(octopeer_observer *op_obs) { return peer_observers.erase(op_obs) > 0; }
+        bool send_query(const octopeer &op, const octoquery &oq) const { return false; }
 };
 
 #endif
