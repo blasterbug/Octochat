d3ee3891f32d354712b599ab2b6f9e44a5df992f
Giraudux <giraudux@gmail.com>
2014-11-14 02:22:33 +0100

    add tcp server functions

--- include/octonet/octonet.hpp
+++ include/octonet/octonet.hpp
@@ -4,9 +4,13 @@
 #define OCTONET_VERSION "1"
 #define OCTONET_HEADER "OCTONET"
 #define APP_HEADER "APP"
-#define CONTENT_LENGTH_HEADER "CONTENT_LENGTH"
 
 #include <set>
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/smart_ptr.hpp>
+#include <boost/thread/thread.hpp>
 
 #include "octoconnect_tcp.hpp"
 #include "octopeer.hpp"
@@ -24,6 +28,7 @@ class octonet
         unsigned short __udp_port;
 
     protected:
+        boost::asio::io_service _io_service;
         void _notify_query_observers(const octoquery &oq) const {}
         void _notify_peer_observers(const octopeer &op) const
         {
@@ -32,7 +37,29 @@ class octonet
 			{
 				(*it)->update_peer(op);
 			}
-		}
+        }
+        void _server_tcp(unsigned short port)
+        {
+                boost::asio::ip::tcp::acceptor a(_io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port));
+                for (;;)
+                {
+                        boost::shared_ptr<boost::asio::ip::tcp::socket> sock(new boost::asio::ip::tcp::socket(_io_service));
+                        a.accept(*sock);
+                        boost::thread t(boost::bind(&octonet::_session_tcp0, this, sock));
+                }
+        }
+        void _session_tcp0(boost::shared_ptr<boost::asio::ip::tcp::socket> sock)
+        {
+                try
+                {
+                        // read data length
+                        // read data
+                }
+                catch (std::exception& e)
+                {
+                        std::cerr << "Exception in thread: " << e.what() << std::endl;
+                }
+        }
 
     public:
         octonet() {}
@@ -42,6 +69,10 @@ class octonet
         bool add_peer_observer(octopeer_observer *op_obs) { return (peer_observers.insert(op_obs)).second; }
         bool rem_peer_observer(octopeer_observer *op_obs) { return peer_observers.erase(op_obs) > 0; }
         bool send_query(const octopeer &op, const octoquery &oq) const { return false; }
+        void _session_tcp(boost::shared_ptr<boost::asio::ip::tcp::socket> sock)
+        {
+                ;
+        }
 };
 
 #endif
