3d10213fee5d18771753443d2b2288330823da9d
Giraudux <giraudux@gmail.com>
2014-11-15 17:34:51 +0100

    remove members and add endpoint member

--- include/octonet/octopeer.hpp
+++ include/octonet/octopeer.hpp
@@ -5,12 +5,8 @@
 
 struct octopeer
 {
-    boost::asio::ip::address ip_address;
-    unsigned short tcp_port;
-    unsigned short udp_port;
-
-    octopeer(boost::asio::ip::address _ip_address, unsigned short _tcp_port, unsigned short _udp_port) : ip_address(_ip_address), tcp_port(_tcp_port), udp_port(_udp_port) {}
-    octopeer(const octopeer &op) : ip_address(op.ip_address), tcp_port(op.tcp_port), udp_port(op.udp_port) {}
+    boost::asio::ip::tcp::endpoint endpoint;
+    octopeer(const octopeer &op) : endpoint(op.endpoint) {}
 };
 
 #endif
