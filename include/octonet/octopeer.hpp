2ac3967e6b4ebe261104e44a1d6e99f818087492
Giraudux <giraudux@gmail.com>
2014-11-16 20:09:41 +0100

    rewrite

--- include/octonet/octopeer.hpp
+++ include/octonet/octopeer.hpp
@@ -5,8 +5,19 @@
 
 struct octopeer
 {
-    boost::asio::ip::tcp::endpoint endpoint;
-    octopeer(const octopeer &op) : endpoint(op.endpoint) {}
+    boost::asio::ip::address ip_address;
+    unsigned short tcp_port;
+
+    octopeer(boost::asio::ip::address _ip_address, unsigned short _tcp_port) : ip_address(_ip_address), tcp_port(_tcp_port) {}
+    octopeer(const octopeer &op) : ip_address(op.ip_address), tcp_port(op.tcp_port) {}
+};
+
+struct octopeer_comparator
+{
+    bool operator()(const octopeer &lhs, const octopeer &rhs) const
+    {
+        return lhs.ip_address < rhs.ip_address;
+    }
 };
 
 #endif
