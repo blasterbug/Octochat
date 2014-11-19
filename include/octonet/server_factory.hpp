b6b71a8925c185f58b6c6da9198c6888360135c1
Giraudux <giraudux@gmail.com>
2014-11-20 00:52:33 +0100

    fix circular includes

--- include/octonet/server_factory.hpp
+++ include/octonet/server_factory.hpp
@@ -4,20 +4,22 @@
 #include "tcp_server.hpp"
 #include "udp_server.hpp"
 
+class octonet_manager;
+
 enum server_type { tcp, udp };
 
 class server_factory
 {
 private:
-    octonet_manager* octonet_manager_;
+    octonet_manager* net_manager_;
 public:
-    server_factory(octonet_manager* _octonet_manager) : octonet_manager_(_octonet_manager) {}
+    server_factory(octonet_manager* _net_manager) : net_manager_(_net_manager) {}
     abstract_server* create_server(server_type _type, unsigned short _port)
     {
-        switch(type)
+        switch(_type)
         {
-            case tcp: return new tcp_server(octonet_manager_, _port);
-            case udp: return new udp_server(octonet_manager_, _port);
+            case tcp: return new tcp_server(net_manager_, _port);
+            case udp: return new udp_server(net_manager_, _port);
         }
     }
 };
