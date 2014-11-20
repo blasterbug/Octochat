c4549726f013cb86e02ee0ff01568bb3294c4201
Giraudux <giraudux@gmail.com>
2014-11-20 19:07:32 +0100

    add virtual pure ip_address function

--- include/octonet/abstract_server.hpp
+++ include/octonet/abstract_server.hpp
@@ -1,12 +1,15 @@
 #ifndef ABSTRACT_SERVER_HPP
 #define ABSTRACT_SERVER_HPP
 
+#include <boost/asio.hpp>
+
 class abstract_server
 {
 public:
     virtual ~abstract_server(void) {}
     virtual void run(void) = 0;
     virtual unsigned short port(void) = 0;
+    virtual boost::asio::ip::address ip_address(void) = 0;
 };
 
 #endif
