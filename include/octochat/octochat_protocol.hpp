c372a6444e744c2b1bf41124d9d54d9d99124df3
blasterbug <blasterbug@users.noreply.github.com>
2014-11-30 20:59:46 +0100

    octochat protocol detailed

--- include/octochat/octochat_protocol.hpp
+++ include/octochat/octochat_protocol.hpp
@@ -35,17 +35,27 @@
 /**
  * Define octochat protocol words
  */
-
-/// headers for query
+ /// for new users
 #define OCTOCHAT_PROTOCOL_NEW_USER "user"
+ /// to include username in octoqueries
 #define OCTOCHAT_PROTOCOL_USER_NAME "nickname"
+ /// when an octo-user is being accepted in a room
 #define OCTOCHAT_PROTOCOL_AUTH_OK "accepted"
+ /// when an user is rejected from a room
 #define OCTOCHAT_PROTOCOL_AUTH_KO "username in used"
+ /// for octomails
 #define OCTOCHAT_PROTOCOL_MAIL "msg"
+ /// when an octo-user quit an octo-room, specialy when he's the owner
+#define OCTOCHAT_PROTOCOL_QUIT "quit"
+ /// to specify octoroom name for octomails and auth
 #define OCTOCHAT_PROTOCOL_DESTINEE "to"
+ /// when changing a room subject
 #define OCTOCHAT_PROTOCOL_SUBJECT "title"
+ /// to specify octorooms owner
 #define OCTOCHAT_PROTOCOL_OWNER "owner"
+ /// to specify octoroom name
 #define OCTOCHAT_PROTOCOL_ROOM "roomname"
+ /// for errors, currently useless
 #define OCTOCHAT_PROTOCOL_ERR "error"
 
 #endif
