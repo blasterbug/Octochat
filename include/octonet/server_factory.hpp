5ba9e38c968b447a876b95916846708b7a29adcf
Giraudux <giraudux@gmail.com>
2014-12-01 01:34:17 +0100

    add some doc

--- include/octonet/server_factory.hpp
+++ include/octonet/server_factory.hpp
@@ -13,12 +13,27 @@ class octonet_manager;
 
 enum server_type { tcp, udp };
 
+/*!
+ * \class server_factory
+ * \brief Classe server_factory, construit des serveurs UDP/TCP.
+ */
 class server_factory
 {
 private:
     octonet_manager* net_manager_;
 public:
+
+    /*!
+     * \brief Constructeur par défaut de server_factory.
+     * \param _net_manager : l'octonet_manager
+     */
     server_factory(octonet_manager* _net_manager) : net_manager_(_net_manager) {}
+
+    /*!
+     * \brief Construit un serveur UDP ou TCP en fonction d'un port d'écoute.
+     * \param _type : le type de serveur
+     * \param _port : le port d'écoute
+     */
     abstract_server* create_server(server_type _type, unsigned short _port)
     {
         switch(_type)
--- src/octonet/udp_server.cpp
+++ src/octonet/udp_server.cpp
@@ -1,3 +1,8 @@
+//          Copyright Alexis Giraudet 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #include "octonet/octonet_manager.hpp"
 #include "octonet/octopeer.hpp"
 #include "octonet/udp_server.hpp"
